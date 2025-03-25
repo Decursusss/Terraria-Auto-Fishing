a6fea0403dc9fbd062ba51d7eda8c8d3c52dab0f
Decursusss <127863168+Decursusss@users.noreply.github.com>
2025-03-25 14:29:52 +0200

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,17 +1,14 @@
 from ultralytics import YOLO
 
-dataset_path = "data.yaml"
-model = YOLO("yolo11n.pt")
+def study()
+  """обучаем YOLO модель на нашем датасете"""
+  dataset_path = "data.yaml"
+  model = YOLO("yolo11n.pt")
+  model.train(data=dataset_path, epochs=150, batch=16, imgsz=640)
 
 
-model.train(data=dataset_path, epochs=150, batch=16, imgsz=640)
-
-
-
-# from ultralytics import YOLO
-#
-# model = YOLO("runs/detect/train7/weights/best.pt")
-#
-# result = model("DataSet/train/images/1_png.rf.5a8f38f04e9bfa722ef812a1d12ee081.jpg", conf=0.1)
-#
-# result[0].show()
+def testCase():
+  """Проверяем новую модель"""
+  model = YOLO("runs/detect/train7/weights/best.pt")
+  result = model("DataSet/train/images/1_png.rf.5a8f38f04e9bfa722ef812a1d12ee081.jpg", conf=0.1)
+  result[0].show()
